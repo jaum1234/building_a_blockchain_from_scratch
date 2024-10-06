974fbe6b27ee39bd319233254d8cccaacbf57767
joao coura <joaovitorsouzacoura@gmai.com>
2024-10-05 23:49:49 -0300

    feat: documenting the Block data structure.

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+/*
+A block is a data structure data will contain valuable
+information.
+
+Different blockchains have varying block specifications.
+
+In the bitcoin specification, Timestamp, PrevBlockHash
+and Hash would be considered as header fields.
+
+For the sake of simplicity, that project will consider a block
+to be as simple as the data structure defined bellow, not following
+any specific specification.
+*/
 type Block struct {
 	Timestamp     int64
 	Data          []byte
