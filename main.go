51c30278a70af48f87737ebbcdedb19355654f66
joao coura <joaovitorsouzacoura@gmai.com>
2024-10-06 00:02:57 -0300

    :sparkles: feat: creating a hash function.

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+)
+
 /*
 A block is a data structure data will contain valuable
 information.
@@ -18,6 +24,14 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
 }
 
 func main() {
