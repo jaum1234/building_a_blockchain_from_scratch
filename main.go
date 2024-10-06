79480167a0ada18e63a07a480b08f9fc9e01f692
joao coura <joaovitorsouzacoura@gmai.com>
2024-10-06 00:10:45 -0300

    :sparkles: feat: defining a function for creating new blocks.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"strconv"
+	"time"
 )
 
 /*
@@ -32,6 +33,13 @@ func (b *Block) SetHash() {
 	hash := sha256.Sum256(headers)
 
 	b.Hash = hash[:]
+}
+
+func newBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+
+	return block
 }
 
 func main() {
